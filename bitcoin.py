0b2a0ab9f9ca3aacecc9237f32b540832f145eb8
tb120 <84480467+tb120@users.noreply.github.com>
2021-05-23 02:15:54 +0900

    Add files via upload

---
+++ bitcoin.py
@@ -0,0 +1,106 @@
+import time
+import pybithumb
+import pyupbit
+
+access = "H41UV5OVGzq3sQg2EKRY3NsnxUeTEd0vGFK75qw4"
+seBTCt = "2r5P2ObcCHI2HFW1NF8Yza1S2Q70xPuOAQCy4J4P"
+
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+
+
+# 로그인
+upbit = pyupbit.Upbit(access, seBTCt)
+print("autotrade start")
+
+# 자동매매 시작
+
+ap = pyupbit.get_current_price("KRW-BTC")
+bp = pyupbit.get_current_price("KRW-BTC")
+
+
+while True:
+    try:
+        time.sleep(1)
+        df = pybithumb.get_ohlcv("BTC")
+        ma5 = df['close'].rolling(5).mean()
+        last_ma5 = ma5[-2]
+        # print("5일 평균가: %s" % last_ma5)
+        current_price = get_current_price("KRW-BTC")
+        # print("현재가: %s" % current_price)
+        if last_ma5 < current_price:
+            # print("상승장")
+
+            if ap > current_price:
+                ap = current_price
+                # ss = ap * 1.02
+                # print("ap:- %s" % (int(ap)))
+
+            if bp < current_price:
+                bp = current_price
+                # xx = bp * 0.99
+                # print("bp:+ %s - 매도예정금액: %s" % (int(bp), int(xx)))
+
+            sp = ap * 1.02
+            xp = bp * 0.98
+
+            # 매수
+            if sp < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    ap = pyupbit.get_current_price("KRW-BTC")
+                    bp = pyupbit.get_current_price("KRW-BTC")
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    # print("매수주문: ", int(krw*0.9995))
+
+                    time.sleep(5)
+            #매도
+            if xp > current_price:
+                btc = get_balance("BTC")
+                if btc > 0.00008:
+                    ap = pyupbit.get_current_price("KRW-BTC")
+                    bp = pyupbit.get_current_price("KRW-BTC")
+                    upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                    # print("매도주문: ", int(btc*0.9995))
+                    time.sleep(5)
+        else:
+            # print("하락장")
+            if bp < current_price:
+                bp = current_price
+                # xx = bp * 0.99
+                # print("bp:+ %s - 매도예정금액: %s" % (int(bp), int(xx)))
+            xp = bp * 0.99
+
+            if xp > current_price:
+                btc = get_balance("BTC")
+                if btc > 0.00008:
+                    ap = pyupbit.get_current_price("KRW-BTC")
+                    bp = pyupbit.get_current_price("KRW-BTC")
+                    upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                    # print("매도주문: ", int(btc*0.9995))
+                    time.sleep(5)
+
+    except Exception as e:
+        print(e)
+        time.sleep(1)
